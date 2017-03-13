4e29f583723ee10fec48bd48b2a3c0291b20995a
Leo Dubert <leodubert@gmail.com>
2017-03-13 18:58:05 +0100

    made routes 4, 5, 6. On 4, the date picker is still not working, which sucks i know...

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,10 @@
 class BookingsController < ApplicationController
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,100 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+users_attributes = [
+  {
+    email:        "yoko@gmail.com",
+    password:     "secret"
+  },
+   {
+    email:        "marcello@gmail.com",
+    password:     "secret"
+  },
+   {
+    email:        "robert@gmail.com",
+    password:     "secret"
+  },
+  {
+    email:        "emile@gmail.com",
+    password:     "secret"
+  },
+  {
+    email:        "boris@gmail.com",
+    password:     "secret"
+  },
+  {
+    email:        "romain@gmail.com",
+    password:     "secret"
+  },
+]
+
+users_attributes.each { |params| User.create!(params) }
+
+Meal.destroy_all
+meals_attributes = [
+  {
+    name:        "Kawai",
+    description: "Sushi selection, vegetarian various types of fish and seafood, sushi prepared in front of you and your guests.",
+    price:       "54",
+    food_type:   "Japanese",
+    user_id:     User.all.sample.id
+  },
+  {
+    name:        "Mama Mia",
+    description: "Bruschetta with fresh diced tomatoes, garlic, oregano, basil and mozzarella. Selection of Italian cheeses; Italian salami. Fillet of beef with tasty cream mushroom. Tiramisù",
+    price:       "40",
+    food_type:   "Italian",
+    user_id:     User.all.sample.id
+  },
+  {
+    name:        "Sweet Sauterne",
+    description: "Sushi selection, vegetarian various types of fish and seafood, sushi prepared in front of you and your guests.",
+    price:       "54",
+    food_type:   "French",
+    user_id:     User.all.sample.id
+  },
+  {
+    name:        "Mezzeh delight",
+    description: "Chicken kofta served with bulgur wheat,kale, sunflower seeds, pumpkin seeds, fresh herbs ,tender stem broccoli and pomegranate seeds with mint cucumber dill natural joghurt dip.",
+    price:       "35",
+    food_type:   "Lebanese",
+    user_id:     User.all.sample.id
+  },
+]
+
+meals_attributes.each { |params| Meal.create!(params) }
+
+Booking.destroy_all
+bookings_attributes = [
+  {
+    date:             "20/03/2017",
+    number_customers: 6,
+    meal_id:          Meal.all.sample.id,
+    user_id:          User.all.sample.id
+  },
+  {
+    date:             "21/03/2017",
+    number_customers: 6,
+    meal_id:          Meal.all.sample.id,
+    user_id:          User.all.sample.id
+  },
+  {
+    date:             "22/03/2017",
+    number_customers: 6,
+    meal_id:          Meal.all.sample.id,
+    user_id:          User.all.sample.id
+  },
+  {
+    date:             "23/03/2017",
+    number_customers: 6,
+    meal_id:          Meal.all.sample.id,
+    user_id:          User.all.sample.id
+  },
+]
+
+bookings_attributes.each { |params| Booking.create!(params) }
+
+puts "Seed OKAYYY"
