ed3bcd66a80da6d81f89b23db6cb3bdf0ec8195b
geoffrey glorian <glorian.geoffrey@gmail.com>
2017-03-13 18:41:35 +0100

    Meal new and create with images

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,24 @@
 class MealsController < ApplicationController
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    @meal.user = current_user
+
+    if @meal.save
+      @meal.save
+      redirect_to meal_path(@meal)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :description, :food_type, :price, :photo)
+  end
 end
