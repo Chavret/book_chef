ef8c4fb05065a19fea2498426acd3af979c5d27b
geoffrey glorian <glorian.geoffrey@gmail.com>
2017-03-14 18:00:43 +0100

    Delete last_name and first_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170314124750) do
-
+ActiveRecord::Schema.define(version: 20170314104546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +66,6 @@ ActiveRecord::Schema.define(version: 20170314124750) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "address"
-<<<<<<< HEAD
     t.string   "provider"
     t.string   "uid"
     t.string   "facebook_picture_url"
@@ -76,10 +73,6 @@ ActiveRecord::Schema.define(version: 20170314124750) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
-=======
-    t.string   "first_name"
-    t.string   "last_name"
->>>>>>> master
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
