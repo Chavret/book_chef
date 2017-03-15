64f2e82f7106e6b7adddacb5a20e31c08c9fe4fb
Leo Dubert <leodubert@gmail.com>
2017-03-15 15:36:32 +0100

    first version of double table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @bookings = @user.bookings
+    @booked = []
+    @booked = Booking.where(meal_id: 11)
   end
 
 end
