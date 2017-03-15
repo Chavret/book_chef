41505f9a3c402af806d8d0c56385eacd2692fdeb
Axelle Renie <axelle.renie@gmail.com>
2017-03-15 15:32:27 +0100

    lots

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,6 +72,13 @@ users_attributes = [
     facebook_picture_url: "http://static.dnaindia.com/sites/default/files/styles/square/public/2015/06/23/349000-putin.jpg",
     email:        "vlady@gmail.com",
     password:     "secret"
+  },
+   {
+    first_name:   "Barack",
+    last_name:    "Obama",
+    facebook_picture_url: "https://ae01.alicdn.com/kf/HTB1F.0bPpXXXXaBXpXXq6xXFXXX8/Vintage-Square-Sunglasses-Men-Women-Oliver-Peoples-OV5316SU-President-font-b-Obama-b-font-Sun-Glasses.jpg",
+    email:        "barack@gmail.com",
+    password:     "secret"
   }
 ]
 
@@ -114,7 +121,7 @@ meals_attributes = [
     name:        "Good Evening England",
     description: "Traditional full English latefast with traditionally back bacon, fried, poached or scrambled eggs, fried or grilled tomatoes, fried mushrooms, fried bread or toast with butter, and sausages. Black pudding, baked beans, bubble and squeak and hash browns are often also included. Fried or grilled oatcakes sometimes replace fried bread. The food is traditionally served with a mug of tea; coffee is an alternative.",
     price:       25,
-    food_type:   "Lebanese",
+    food_type:   "British",
     user:        User.where(last_name: "Thatcher").first,
     photo_url:   "http://www.anecocatering.co.uk/wp-content/uploads/2015/06/breakfast-pan.jpg"
   },
@@ -122,7 +129,7 @@ meals_attributes = [
     name:        "Light Bites",
     description: "Salmon carpaccio and sweet corn pan cake. Vegetables brunoise, roasted king prawns and fresh pasta. Roasted apricots and almonds ice cream.",
     price:       79,
-    food_type:   "Paleo",
+    food_type:   "American",
     user:        User.where(last_name: "Jolie").first,
     photo_url:   "http://www.tablecrafter.com/uploads/ckeditor/pictures/418/content_xclass_master_class_speedlight_demo2_1.jpg"
   },
@@ -142,37 +149,92 @@ meals_attributes = [
     user:        User.where(last_name: "Hitchcock").first,
     photo_url:   "https://static1.squarespace.com/static/5220aad8e4b079c14a333578/56cf8f5bf699bb26fa0deeeb/56cf96a6b654f9decd164c73/1456445300874/Loiseau+les+Ducs+dish.jpg"
   },
+   {
+    name:        "Meet my Meat",
+    description: "lot of meat",
+    price:       90,
+    food_type:   "American",
+    user:        User.where(last_name: "Hemingway").first,
+    photo_url:   "http://athome.kimvallee.com/wp-content/uploads/2012/10/familystyle.jpg"
+  },
+  {
+    name:        "Even Lighter Bites",
+    description: "Almost nothing to eat. I will watch out your weight !",
+    price:       50,
+    food_type:   "American",
+    user:        User.where(last_name: "Jolie").first,
+    photo_url:   "http://terrymcdonagh.com/wp-content/gallery/irish-food-photography/Fruit-salad-1.jpg"
+  },
+  {
+    name:        "Fish but no Chips",
+    description: "Real fish the French way",
+    price:       69,
+    food_type:   "French",
+    user:        User.where(last_name: "Picasso").first,
+    photo_url:   "http://www.goodeyeblog.com/wp-content/uploads/2013/10/1787_d800b_Maso_Restaurant_Westin_Hotel_San_Francisco_Food_Photography_web(pp_w1200_h801).jpg"
+  },
+  {
+    name:        "Roasted Trump",
+    description: "Huge ",
+    price:       50,
+    food_type:   "American",
+    user:        User.where(last_name: "Obama").first,
+    photo_url:   "http://www.thephoblographer.com/wp-content/uploads/2014/11/Lou-Manna_Turkey.jpg"
+  },
+  {
+    name:        "Kids'mania",
+    description: "Sweet sweet and only sweets",
+    price:       20,
+    food_type:   "American",
+    user:        User.where(last_name: "Jolie").first,
+    photo_url:   "http://taphotos.com/wp-content/uploads/2015/06/food-photography.jpg"
+  },
+  {
+    name:        "Je suis Rital et le reste",
+    description: "Pizza",
+    price:       50,
+    food_type:   "Italian",
+    user:        User.where(last_name: "Berlusconi").first,
+    photo_url:   "http://foodportfolio.com/wordpress/wp-content/pittsburgh-food-photographer/food-photography-test-shoot-1.jpg"
+  },
+  {
+    name:        "Burger my world",
+    description: "Fat only",
+    price:       31,
+    food_type:   "American",
+    user:        User.where(last_name: "Obama").first,
+    photo_url:   "http://www.reason-marketing.com/gallery/Why-you-should-hire-a-professional-food-photographer-for-your-restaurant-picture/Why-you-should-hire-a-professional-food-photographer-for-your-restaurant.jpg"
+  },
+  {
+    name:        "Delicatesses",
+    description: "French",
+    price:       50,
+    food_type:   "American",
+    user:        User.where(last_name: "Picasso").first,
+    photo_url:   "https://static1.squarespace.com/static/56944c561c121086a8898a53/t/56b299ba22482e9cac1e41e7/1454545339432/food3.jpg?format=1500w"
+  },
+  {
+    name:        "Grand-Mother dinner",
+    description: "old stuff",
+    price:       50,
+    food_type:   "French",
+    user:        User.where(last_name: "Thatcher").first,
+    photo_url:   "https://s-media-cache-ak0.pinimg.com/originals/5b/1f/35/5b1f35e53451489f3d522327f62d20f3.jpg"
+  }
 ]
 
 meals_attributes.each { |params| Meal.create!(params) }
 
-bookings_attributes = [
-  {
-    date:             "20/03/2017",
-    number_customers: 6,
-    meal_id:          Meal.all.sample.id,
-    user_id:          User.all.sample.id
-  },
-  {
-    date:             "21/03/2017",
-    number_customers: 6,
-    meal_id:          Meal.all.sample.id,
-    user_id:          User.all.sample.id
-  },
-  {
-    date:             "22/03/2017",
-    number_customers: 6,
-    meal_id:          Meal.all.sample.id,
-    user_id:          User.all.sample.id
-  },
-  {
-    date:             "23/03/2017",
-    number_customers: 6,
-    meal_id:          Meal.all.sample.id,
-    user_id:          User.all.sample.id
-  }
-]
+500.times  do
+  date = rand(1..90).days.from_now.strftime("%d/%m/%Y")
+  number_customers = rand(2..9)
 
-bookings_attributes.each { |params| Booking.create!(params) }
+  Booking.create!({
+    date:             date,
+    number_customers: number_customers,
+    meal_id:          Meal.all.sample.id,
+    user_id:          User.all.sample.id
+})
+end
 
 puts "Seed OKAYYY"
